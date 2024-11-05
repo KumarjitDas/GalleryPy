159169c839f29f2f28f6b2e9d5ace6d0eaadd713
kumarjit <kumarjitdas1999@gmail.com>
2024-11-05 13:35:47 +0530

    Initial commit.

---
+++ GalleryPy.py
@@ -0,0 +1,20 @@
+# import tkinter as tk
+
+
+def print_application_information():
+    message = """
+    GalleryPy
+    
+    A sleek and intuitive Image Viewer GUI application. Easily view, navigate, and explore images with smooth
+    performance and a user-friendly interface.
+    """
+
+    print(message)
+
+
+def main():
+    print_application_information()
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,3 @@
+# GalleryPy
+
+A sleek and intuitive Image Viewer GUI application. Easily view, navigate, and explore images with smooth performance and a user-friendly interface.
