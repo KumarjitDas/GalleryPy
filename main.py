2204abcd71354c1985058d75bea76dd555b7fe95
KumarjitDas <kumarjitdas1999@gmail.com>
2024-11-14 15:21:43 +0530

    Added functionality to open and show image file. Added command-line arguments. Added a test image resource.

--- main.py
+++ main.py
@@ -1,14 +1,70 @@
+import argparse
+import os
 import platform
 from pathlib import Path
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 
 class App:
+    APP_WIDTH = 800
+    APP_HEIGHT = 600
     APP_NAME = "GalleryPy"
-    APP_GEOMETRY = "640x400"
+    APP_GEOMETRY = str(APP_WIDTH) + "x" + str(APP_HEIGHT)
 
-    def __init__(self):
+    APP_SUPPORTED_IMAGE_FILE_EXTENSIONS = [
+        'bmp',  # BMP
+        'dib',  # BMP
+        'eps',  # EPS
+        'gif',  # GIF
+        'icns',  # ICNS
+        'ico',  # ICO
+        'im',  # IM
+        'jpeg',  # JPEG
+        'jpe',  # JPEG
+        'jpg',  # JPEG
+        'msp',  # MSP
+        'pcx',  # PCX
+        'png',  # PNG
+        'ppm',  # PPM
+        'pgm',  # PPM
+        'pbm',  # PPM
+        'pnm',  # PPM
+        'sgi',  # SGI
+        'rgb',  # SGI
+        'bw',  # SGI
+        'spider',  # SPIDER
+        'tga',  # TGA
+        'tif',  # TIFF
+        'tiff',  # TIFF
+        'webp',  # WEBP
+        'xbm',  # XBM
+        'pdf',  # PDF
+        'psd',  # PSD
+        'fits',  # FITS
+        'fit',  # FITS
+        'pcd',  # PCD
+        'dds',  # DDS
+    ]
+
+    def __init__(self, arguments):
+        if arguments is None:
+            arguments = {
+                "imageFile": None,
+                "imagesDirectory": None,
+                "verbose": False,
+                "config": None
+            }
+
+        self.cmd_imageFile = arguments["imageFile"]
+        self.cmd_imagesDirectory = arguments["imagesDirectory"]
+        self.cmd_verbose = arguments["verbose"]
+        self.cmd_config = arguments["config"]
+
+        self.current_image = None
+        self.current_photo = None
+
         self.root = tk.Tk()
         self.root_style = ttk.Style()
 
@@ -60,6 +116,8 @@ class App:
             # Linux or other Unix-like
             self.pictures_directory += "/Pictures"
 
+        self.pictures_directory = os.path.abspath(self.pictures_directory)
+
     def assign_navigation(self, navigation):
         if navigation is None:
             navigation = {}
@@ -75,7 +133,29 @@ class App:
             child.destroy()
 
     def run(self):
-        self.determine_pictures_directory()
+        if self.cmd_imagesDirectory is None:
+            self.determine_pictures_directory()
+        else:
+            # noinspection PyTypeChecker
+            self.pictures_directory = os.path.abspath(self.cmd_imagesDirectory)
+
+        if self.cmd_imageFile is not None:
+            # noinspection PyTypeChecker
+            image_file_path = os.path.abspath(self.cmd_imageFile)
+            self.pictures_directory = os.path.dirname(image_file_path)
+            self.current_image = Image.open(image_file_path)
+            self.current_photo = ImageTk.PhotoImage(self.current_image)
+
+            print("Format:", self.current_image.format)
+            print("Size:", self.current_image.size)
+            print("Mode:", self.current_image.mode)
+
+            canvas = tk.Canvas(self.main_frame, width=self.current_image.width, height=self.current_image.height)
+            canvas.pack()
+            canvas.create_image(0, 0, anchor=tk.NW, image=self.current_photo)
+
+            self.root.title(self.APP_NAME + " - " + os.path.splitext(os.path.basename(image_file_path))[0])
+
         self.set_status_bar_path(self.pictures_directory)
         self.root.mainloop()
 
@@ -89,7 +169,23 @@ class App:
 
 
 def main():
-    app = App()
+    parser = argparse.ArgumentParser(description="GalleryPy")
+
+    parser.add_argument("image-file", nargs='?', help="Path to a image file")
+    parser.add_argument("-f", "--image-file", type=str, help="Path to a image file")
+    parser.add_argument("-d", "--images-directory", type=str, help="Path to a images directory")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("-c", "--config", type=str, help="Path to the configuration file")
+
+    args = parser.parse_args()
+
+    app = App({
+        "imageFile": args.image_file,
+        "imagesDirectory": args.images_directory,
+        "verbose": args.verbose,
+        "config": args.config
+    })
+
     print(app)
     app.run()
 
