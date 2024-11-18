c010458103ac807a6d69341be865bbfb9cdb00c2
kumarjit <kumarjitdas1999@gmail.com>
2024-11-18 23:52:25 +0530

    Made the resizing event more robust. Added dynamic sizing for home page items.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import platform
 from pathlib import Path
 import tkinter as tk
 from tkinter import ttk
+from tkinter.font import Font
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
@@ -54,6 +55,12 @@ class App:
         'dds',  # DDS
     ]
 
+    EMPTY_FOLDER_IMAGE_SIZE_MIN = 60
+    EMPTY_FOLDER_IMAGE_SIZE_MAX = 120
+
+    EMPTY_FOLDER_LABEL_FONT_SIZE_MIN = 8
+    EMPTY_FOLDER_LABEL_FONT_SIZE_MAX = 11
+
     def __init__(self, arguments):
         if arguments is None:
             arguments = {
@@ -70,12 +77,17 @@ class App:
         self.container = tk.Frame(master=self.root)
         self.windowing_system = self.root.tk.call('tk', 'windowingsystem')
         self.pages = {}
+        self.page_items = {}
         self.frames = {}
         self.canvases = {}
+        self.labels = {}
+        self.fonts = {}
         self.images = {}
         self.photos = {}
         self.screen_width = self.root.winfo_screenwidth()
         self.screen_height = self.root.winfo_screenheight()
+        self.app_width = self.root.winfo_width()
+        self.app_height = self.root.winfo_height()
 
         self.dialog_filetypes = (
             ('Image Files', ' '.join([f'*.{ext}' for ext in self.APP_SUPPORTED_IMAGE_FILE_EXTENSIONS])),
@@ -88,6 +100,7 @@ class App:
         self.apply_platform_styles()
         self.create_page_home()
         self.create_page_img()
+        self.create_page_img_error()
         self.create_page_dirs_files()
 
         self.root.title(self.APP_NAME)
@@ -146,23 +159,101 @@ class App:
         frame = tk.Frame(master=self.container)
         frame.grid(row=0, column=0, sticky=tk.NSEW)
 
-        center_frame = ttk.Frame(master=frame)
+        center_frame = ttk.Frame(master=frame, width=50, height=50)
         center_frame.place(relx=0.5, rely=0.45, anchor=tk.CENTER)
 
-        image_size = 160
+        empty_folder_canvas = tk.Canvas(master=center_frame, cursor='hand2')
+        empty_folder_canvas.pack(side=tk.TOP, anchor=tk.S, fill=tk.BOTH, expand=True)
+
+        empty_folder_label_font = Font(family='Helvetica', size=self.EMPTY_FOLDER_LABEL_FONT_SIZE_MIN, weight='bold')
+
+        empty_folder_label = ttk.Label(
+            master=center_frame,
+            text="No image file/directory have been chosen.\nOpen file  Ctrl + O",
+            anchor=tk.CENTER,
+            justify=tk.CENTER,
+            # wraplength=240,
+            padding=2,
+            font=empty_folder_label_font,
+            cursor='hand2'
+        )
+        empty_folder_label.pack(side=tk.BOTTOM, anchor=tk.N, fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+        self.canvases['empty_folder'] = empty_folder_canvas
+        self.fonts['empty_folder'] = empty_folder_label_font
+        self.labels['empty_folder'] = empty_folder_label
+        self.page_items['home'] = self.load_page_home_items
+        self.pages['home'] = frame
+        self.frames['home'] = frame
+
+        empty_folder_canvas.bind('<Button-1>', self.on_home_page_click)
+        empty_folder_label.bind('<Button-1>', self.on_home_page_click)
+        # frame.bind('<Configure>', self.on_home_page_frame_resize)
+
+    def create_page_img(self):
+        frame = tk.Frame(self.container)
+        frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+        main_img_view_canvas = tk.Canvas(master=frame, bg='yellow')
+        main_img_view_canvas.pack(side=tk.TOP, anchor=tk.N, fill=tk.BOTH, expand=True)
+
+        self.canvases['main_img_view'] = main_img_view_canvas
+        self.page_items['img'] = self.load_page_img_items
+        self.pages['img'] = frame
+        self.frames['img'] = frame
+
+    def create_page_img_error(self):
+        frame = tk.Frame(self.container)
+        frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+        label = ttk.Label(frame, text="Could not show image.", background="red", foreground="white")
+        label.pack(side=tk.TOP, anchor=tk.N, fill=tk.X, expand=True)
+
+        self.page_items['img_error'] = self.load_page_img_error_items
+        self.pages['img_error'] = frame
+        self.frames['img_error'] = frame
+
+    def create_page_dirs_files(self):
+        frame = tk.Frame(self.container)
+        frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+        label = ttk.Label(frame, text="Welcome", background="red", foreground="white")
+        label.pack(side=tk.BOTTOM, anchor=tk.S, fill=tk.X, expand=True)
+
+        self.page_items['dirs_files'] = self.load_page_dirs_files_items
+        self.pages['dirs_files'] = frame
+        self.frames['dirs_files'] = frame
+
+    def show_page(self, page_name):
+        frame = self.pages.get(page_name)
+        page_item_fn = self.page_items.get(page_name)
+
+        if frame:
+            frame.tkraise()
+            page_item_fn()
+        else:
+            print(f"Page '{page_name}' does not exist.")
+
+    def load_page_home_items(self):
+        image_size = self.EMPTY_FOLDER_IMAGE_SIZE_MAX
         empty_folder_image_path = os.path.join(self.APP_IMAGES_PATH, 'empty-folder.png')
         empty_folder_image = Image.open(empty_folder_image_path).resize((image_size, image_size), Image.Resampling.LANCZOS)
         empty_folder_photo = ImageTk.PhotoImage(empty_folder_image)
 
-        empty_folder_canvas = tk.Canvas(master=center_frame, width=image_size, height=image_size)
-        empty_folder_canvas.pack(fill=tk.BOTH, expand=True)
+        empty_folder_canvas = self.canvases.get('empty_folder')
+        empty_folder_canvas.config(width=image_size, height=image_size)
 
         canvas_width = empty_folder_canvas.winfo_width()
         canvas_height = empty_folder_canvas.winfo_height()
         canvas_width = image_size if canvas_width < image_size else canvas_width
         canvas_height = image_size if canvas_height < image_size else canvas_height
 
-        empty_folder_canvas.create_image(canvas_width // 2, canvas_height // 2, anchor=tk.CENTER, image=empty_folder_photo)
+        empty_folder_canvas.create_image(
+            canvas_width // 2,
+            canvas_height // 2,
+            anchor=tk.CENTER,
+            image=empty_folder_photo
+        )
         empty_folder_canvas.image = empty_folder_photo
 
         self.canvases['empty_folder'] = empty_folder_canvas
@@ -171,32 +262,7 @@ class App:
 
         self.resize_home_page_items()
 
-        label2 = ttk.Label(
-            master=center_frame,
-            text="Label 2 as fasd fjsldfsdlfk asdfasdf asfd asdf jalskfdjs",
-            anchor=tk.CENTER,
-            justify=tk.CENTER,
-            wraplength=240,
-            padding=2,
-            # foreground='#212121',
-            # background='#FFFFFF',
-        )
-        label2.pack(fill=tk.BOTH, expand=True)
-
-        empty_folder_canvas.configure(cursor='hand2')
-        label2.configure(cursor='hand2')
-
-        empty_folder_canvas.bind('<Button-1>', self.on_home_page_click)
-        label2.bind('<Button-1>', self.on_home_page_click)
-        frame.bind('<Configure>', self.on_home_page_frame_resize)
-
-        self.pages['home'] = frame
-        self.frames['home'] = frame
-
-    def create_page_img(self):
-        frame = tk.Frame(self.container)
-        frame.grid(row=0, column=0, sticky=tk.NSEW)
-
+    def load_page_img_items(self):
         if self.current_image_file_path is None:
             print('No image')
         else:
@@ -219,40 +285,24 @@ class App:
             #
             # self.resize_home_page_items()
 
-        self.pages['img'] = frame
-        self.frames['img'] = frame
+    def load_page_img_error_items(self):
+        pass
 
-    def create_page_dirs_files(self):
-        frame = tk.Frame(self.container)
-        frame.grid(row=0, column=0, sticky=tk.NSEW)
+    def load_page_dirs_files_items(self):
+        pass
 
-        label = ttk.Label(frame, text="Welcome", background="red", foreground="white")
-        label.pack(side=tk.BOTTOM, anchor=tk.S, fill=tk.X, expand=True)
-
-        self.pages['dirs_files'] = frame
-        self.frames['dirs_files'] = frame
-
-    def show_page(self, page_name):
-        frame = self.pages.get(page_name)
-
-        if frame:
-            frame.tkraise()
-        else:
-            print(f"Page '{page_name}' does not exist.")
-
-    def resize_home_page_items(self):
-        empty_folder_image = self.images['empty_folder']
-        empty_folder_photo = self.photos['empty_folder']
+    def _resize_home_page_items_(self):
+        empty_folder_image = self.images.get('empty_folder')
+        empty_folder_photo = self.photos.get('empty_folder')
 
         if empty_folder_image is None or empty_folder_photo is None:
             return
 
         empty_folder_canvas = self.canvases['empty_folder']
+        empty_folder_canvas_width = empty_folder_canvas.winfo_width()
+        empty_folder_canvas_height = empty_folder_canvas.winfo_height()
 
-        if empty_folder_canvas.winfo_width() > 0 and empty_folder_canvas.winfo_height() > 0:
-            empty_folder_canvas_width = empty_folder_canvas.winfo_width()
-            empty_folder_canvas_height = empty_folder_canvas.winfo_height()
-
+        if empty_folder_canvas_width > 0 and empty_folder_canvas_height > 0:
             empty_folder_image_ratio = empty_folder_image.width / empty_folder_image.height
             empty_folder_canvas_ratio = empty_folder_canvas_width / empty_folder_canvas_height
 
@@ -272,7 +322,7 @@ class App:
             resized_image = empty_folder_image.resize((new_width, new_height), Image.Resampling.LANCZOS)
             empty_folder_photo = ImageTk.PhotoImage(resized_image)
 
-            empty_folder_canvas.delete("all")
+            empty_folder_canvas.delete('all')
 
             center_x = empty_folder_canvas_width // 2
             center_y = empty_folder_canvas_height // 2
@@ -280,22 +330,63 @@ class App:
             empty_folder_canvas.create_image(center_x, center_y, image=empty_folder_photo, anchor=tk.CENTER)
             self.photos['empty_folder'] = empty_folder_photo
 
+    def resize_home_page_items(self, event=None):
+        empty_folder_image = self.images.get('empty_folder')
+        empty_folder_photo = self.photos.get('empty_folder')
+
+        if empty_folder_image is None or empty_folder_photo is None:
+            return
+
+        empty_folder_canvas = self.canvases['empty_folder']
+        empty_folder_font = self.fonts['empty_folder']
+
+        root_width = self.root.winfo_width()
+        root_height = self.root.winfo_height()
+
+        desired_size = int(min(root_width, root_height) * 0.2)
+        desired_size = max(self.EMPTY_FOLDER_IMAGE_SIZE_MIN, min(desired_size, self.EMPTY_FOLDER_IMAGE_SIZE_MAX))
+
+        desired_font_size = int(min(root_width, root_height) * 0.02)
+        desired_font_size = max(self.EMPTY_FOLDER_LABEL_FONT_SIZE_MIN, min(desired_font_size, self.EMPTY_FOLDER_LABEL_FONT_SIZE_MAX))
+
+        empty_folder_canvas.config(width=desired_size, height=desired_size)
+        empty_folder_font.config(size=desired_font_size)
+
+        resized_image = empty_folder_image.resize((desired_size, desired_size), Image.Resampling.LANCZOS)
+        empty_folder_photo = ImageTk.PhotoImage(resized_image)
+
+        empty_folder_canvas.delete('all')
+
+        empty_folder_canvas.create_image(
+            empty_folder_canvas.winfo_width() // 2,
+            empty_folder_canvas.winfo_height() // 2,
+            image=empty_folder_photo,
+            anchor=tk.CENTER
+        )
+
+        self.photos['empty_folder'] = empty_folder_photo
+
     def on_minimize(self, event):
         if self.root.state() == 'iconic':
-            print("Window minimized")
+            print('Window minimized')
 
     def on_restore(self, event):
         if self.root.state() == 'normal':
-            print("Window restored")
+            print('Window restored')
+            self.resize_home_page_items()
 
     def on_configure(self, event):
         if self.root.state() == 'zoomed':
-            print("Window maximized")
+            print('Window maximized')
+            self.resize_home_page_items()
         elif self.root.state() == 'normal':
-            pass
+            print('Window size configured')
 
-    def on_home_page_frame_resize(self, event):
-        self.resize_home_page_items()
+            if (self.app_width != event.width) or (self.app_height != event.height):
+                self.app_width = event.width
+                self.app_height = event.height
+                print('Resizing...')
+                self.resize_home_page_items()
 
     def on_home_page_click(self, event):
         selected_image_file = filedialog.askopenfilename(
